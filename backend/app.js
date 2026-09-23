4b0f9015a1ec8d2f316ba6103c09c312ad0feb21
Opemipo01-tech <ahmad001mahmud@gmail.com>
2026-09-23 12:27:48 +0100

    fixed the bug in cors

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://messenger-alpha-black.vercel.app/",
+      "https://messenger-alpha-black.vercel.app",
     ],
   })
 );
